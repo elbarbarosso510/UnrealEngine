5651bc97ec3084af5abffdad538dfce14d4e7a31
elbarbarosso510 <el_barbarosso510@protonmail.com>
2024-07-31 23:54:42 +0300

    Fix CSS issue

---
+++ Common/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using ChampionshipMvc3.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ChampionshipMvc3.Models.Interfaces;
+using ChampionshipMvc3.Models.Repositories;
+using ChampionshipMvc3.Models.DataContext;
+using System.IO;
+using ChampionshipMvc3.Models.ViewModels;
+using ChampionshipMvc3.Common;
+using System.Collections;
+
+namespace ChampionshipMvc3.Common
+{
+    public static class ExtensionMethods
+    {
+        
+
+    }
+}
